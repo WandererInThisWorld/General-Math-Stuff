6379ce6906435cedaab0ac1aac27999bffb6a872
kevin sony <kevinsony2003@gmail.com>
2023-12-03 09:12:33 -0600

    moidified script

--- numerical_methods/numerical_computations/opencl/script.sh
+++ numerical_methods/numerical_computations/opencl/script.sh
@@ -8,8 +8,8 @@ else
 		echo "Compiling from Mac"
 		# clang $2 -framework OpenCL -D CL_HPP_TARGET_OPENCL_VERSION=100 -o hello
 		clang $2 -framework OpenCL -D CL_HPP_TARGET_OPENCL_VERSION=100	
-	elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-		gcc -Wall -Wextra -D CL_TARGET_OPENCL_VERSION=100 temp.c -o HelloOpenCL -lOpenCL			gcc -Wall -Wextra -D CL_TARGET_OPENCL_VERSION=100 temp.c -o HelloOpenCL -lOpenCL
+	elif [[ "$OSTYPE" == "linux-gnu"* ]]; then	
+		echo "Compiling from Linux"
 		gcc -Wall -Wextra -D CL_TARGET_OPENCL_VERSION=100 temp.c -lOpenCL	
 	fi
 fi
